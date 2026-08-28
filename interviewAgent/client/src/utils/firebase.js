9af466391b0e6cd580a88792fcd6d9cdf0a1fcf0
XYZthedurgeshtiwari <tdurgesh448@gmail.com>
2026-08-29 00:36:39 +0530

    Update Firebase config to match interviewiq-da893 project

--- interviewAgent/client/src/utils/firebase.js
+++ interviewAgent/client/src/utils/firebase.js
@@ -1,21 +1,18 @@
 
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-4daf5.firebaseapp.com",
-  projectId: "interviewiq-4daf5",
-  storageBucket: "interviewiq-4daf5.firebasestorage.app",
-  messagingSenderId: "672718669623",
-  appId: "1:672718669623:web:8ac42600255a186f1bbcb2"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY || "AIzaSyCaVuVfoRmvM4mu-_fOeo-YpZjAThMPzts",
+  authDomain: "interviewiq-da893.firebaseapp.com",
+  projectId: "interviewiq-da893",
+  storageBucket: "interviewiq-da893.firebasestorage.app",
+  messagingSenderId: "1050386277002",
+  appId: "1:1050386277002:web:b8ad3947a397795a168373"
 };
 
-
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-const auth = getAuth(app)
-
-const provider = new GoogleAuthProvider()
-
-export {auth,provider}
+export { auth, provider };
